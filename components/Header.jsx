c1e523a373e3cd25f828734ceeb858c55198ae3f
Adam <marwan2ouenza@gmail.com>
2025-03-02 22:30:47 -0500

    Reste des animations

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,28 +1,29 @@
-// Code pour afficher l'en-tête de l'application
+"use client";
 
-// Importation du composant Image de Next.js, du logo et des styles CSS
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styles from "./Header.module.css";
-
 import logo from "@/public/logo.png";
 
 export default function Header() {
+    const pathname = usePathname();
+
+    const isActive = (path) => pathname === path ? styles.active : "";
+
     return (
         <header className={styles.header}>
-            {/* Logo cliquable pour retourner à la page d'accueil */}
             <div className={styles.logoContainer}>
                 <Link href="/">
                     <Image src={logo} alt="Logo" width={100} height={100} />
                 </Link>
             </div>
-            {/* Menu de navigation pour accéder aux différentes pages de l'application */}
             <div className={styles.content}>
                 <h1>Esports Hub</h1>
                 <nav className={styles.nav}>
-                    <Link href="/" className={styles.navLink}>Accueil</Link>
-                    <Link href="/Evenements" className={styles.navLink}>Événements</Link>
-                    <Link href="/contact" className={styles.navLink}>Contact</Link>
+                    <Link href="/" className={`${styles.navLink} ${isActive("/")}`}>Accueil</Link>
+                    <Link href="/Evenements" className={`${styles.navLink} ${isActive("/Evenements")}`}>Événements</Link>
+                    <Link href="/contact" className={`${styles.navLink} ${isActive("/contact")}`}>Contact</Link>
                 </nav>
             </div>
         </header>
